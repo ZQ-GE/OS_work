9d081295a6bdff9473f40a2f7850282c6d9aa1a5
ZQ <936856296.com>
2022-03-23 11:16:26 +0800

    job3 promoted

--- job3/mysys.c
+++ job3/mysys.c
@@ -3,6 +3,7 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<sys/types.h>
+#include<string.h>
 
 
 void mysys(char *command)
@@ -10,16 +11,52 @@ void mysys(char *command)
     //实现该函数，该函数执行一条命令，并等待该命令执行结束
 	if(command == NULL)
 	{
-		puts("command error");
+		puts("command error1");
 		exit(1);
 	}
+	
+	char *argvt[10];
+
+	char *token;
+	
+	char s[2] = " ";
+	int i = 0;
+	//main（）样例中，直接将具体字符串传入函数，会导致command缺少 ，需要调用strcpy（）
+	char *copy_command = (char *)malloc(100);
+	strcpy(copy_command, command);
+	
+	// puts(copy_command);
+	
+	token = strtok(copy_command, s);
+	
+	
+	while( token != NULL ) 
+	{
+      		argvt[i] = token;
+			
+		// puts(token);
+		i++;
+
+      		token = strtok(NULL, s);
+   	}
+	
+	argvt[i] = NULL;
 	pid_t pid;
 	pid = fork();
+	
 	if(pid == 0)
 	{
-		execlp("sh", "sh", "-c", command, NULL);
+		
+
+		int error = execvp(argvt[0], argvt);
+		// if(error < 0)
+		//	puts("-------1");
+		// else
+		//	puts("33333333333333");
+		//execlp("sh", "-c", command, NULL);
 			//if(tag < 0)
 			//	puts("command error");
+		
 		exit(1);
 	}
 	else if(pid > 0)
@@ -31,7 +68,7 @@ void mysys(char *command)
 			//exit(0);
 		else
 		{
-			puts("command error");
+			puts("command error2");
 			//exit(1);
 		}
 	}
--- job3/sh2.c
+++ job3/sh2.c
@@ -17,7 +17,6 @@ int build_in(char *command)
 	}
 
 	// pwd
-	// exec can also do it, but may have some problems
 	if(strcmp(command, "pwd") == 0)
         {
                 //puts("pwd success");
@@ -47,54 +46,16 @@ void mysys(char *command)
     //实现该函数，该函数执行一条命令，并等待该命令执行结束
 	if(command == NULL)
 	{
-		puts("command error1");
+		puts("command error");
 		exit(1);
 	}
-	
-	char *argvt[10];
-
-	char *token;
-	
-	char s[2] = " ";
-	int i = 0;
-	// char *copy_command = (char *)malloc(100);
-	// strcpy(copy_command, command);
-	
-	//从标准输入读入的command带有'\0',不需要再进行strcpy（）
-	//
-	// puts(copy_command);
-	
-	//// split
-	token = strtok(command, s);
-	
-	
-	while( token != NULL ) 
-	{
-      		argvt[i] = token;
-			
-		// puts(token);
-		i++;
-
-      		token = strtok(NULL, s);
-   	}
-	
-	argvt[i] = NULL;
 	pid_t pid;
 	pid = fork();
-	
-	if(pid == 0)
+	if(pid == 0) 
 	{
-		
-
-		int error = execvp(argvt[0], argvt);
-		// if(error < 0)
-		//	puts("-------1");
-		// else
-		//	puts("33333333333333");
-		//execlp("sh", "-c", command, NULL);
+		execlp("sh", "sh", "-c", command, NULL);
 			//if(tag < 0)
 			//	puts("command error");
-		
 		exit(1);
 	}
 	else if(pid > 0)
@@ -106,7 +67,7 @@ void mysys(char *command)
 			//exit(0);
 		else
 		{
-			puts("command error2");
+			puts("command error");
 			//exit(1);
 		}
 	}
@@ -116,12 +77,12 @@ void mysys(char *command)
 int main()
 {
 	int tag = 1;
-	char buf[2]="> ";
+	char buf[1]=">";
 	char command[50];
 
 	while(tag)
 	{
-		write(1,buf,2);
+		write(1,buf,1);
 		//gets(command);
 		fgets(command, 50, stdin);
 		char *find;
